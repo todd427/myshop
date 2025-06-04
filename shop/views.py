ae999faea9fef40f1eaf8ecc9ac5fb5ef49037f3
todd427 <todd@toddwriter.com>
2025-06-04 10:20:55 -0700

    page 655

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Category, Product
+from django.shortcuts import get_object_or_404, render,redirect
+from django.views.decorators.http import require_POST
+from shop.models import Product, Category
+from cart.forms import CartAddProductForm
+
 
 def product_list(request, category_slug=None):
     category = None
@@ -20,4 +23,12 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    
+    cart_product_form = CartAddProductForm()
+    return render(
+        request, 
+        'shop/product/detail.html', 
+        {'product': product, 'cart_product_form': cart_product_form}
+        )
+
+
