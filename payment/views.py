687627f354cc5af50f3b8ea290f07357f0777d65
todd427 <todd427@gmail.com>
2025-06-06 17:00:58 -0700

    page 747

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from decimal import Decimal
 import stripe
 from django.conf import settings
@@ -10,6 +9,7 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
 def payment_process(request):
+    print("in payment_process")
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
     if request.method == 'POST':
